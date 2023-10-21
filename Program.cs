3b7f2c9007d84198d2ec0aa57d4ab51f59f72712
Nikolay_Bondarenko <Nikolayb33@MacBook-Air-mac.local>
2023-10-21 19:27:57 +0300

    add answer at task 64

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+// Task 64
+// Задайте значение N. Напишите программу, которая 
+//выведет все натуральные числа в промежутке от N до 1.
+//  Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumbers(int start)
+{
+    if(start == 1)
+    {
+        return start.ToString();
+    }
+
+    return start + " " + PrintNumbers(start - 1); 
+}
+
+System.Console.WriteLine(PrintNumbers(num));
