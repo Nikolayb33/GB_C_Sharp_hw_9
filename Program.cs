4e47d95d7af6af765876f3a03b318653a3b49a6c
Nikolay_Bondarenko <Nikolayb33@MacBook-Air-mac.local>
2023-10-21 19:36:37 +0300

    add answer at task 66

--- Program.cs
+++ Program.cs
@@ -8,17 +8,47 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Write("Enter a number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-string PrintNumbers(int start)
+// string PrintNumbers(int start)
+// {
+//     if(start == 1)
+//     {
+//         return start.ToString();
+//     }
+
+//     return start + " " + PrintNumbers(start - 1); 
+// }
+
+// System.Console.WriteLine(PrintNumbers(num));
+
+// Task 66
+//Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Enter a number M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter a number N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+int SumOfRange(int num1, int num2)
 {
-    if(start == 1)
+    sum += num1;
+    if(num1 == num2)
     {
-        return start.ToString();
+        return sum;
     }
-
-    return start + " " + PrintNumbers(start - 1); 
+    else
+    {
+        return SumOfRange(num1 + 1, num2);
+    }
 }
 
-System.Console.WriteLine(PrintNumbers(num));
+System.Console.WriteLine(SumOfRange(numM, numN));
